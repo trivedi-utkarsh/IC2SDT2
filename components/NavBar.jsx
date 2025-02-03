f39a0e33a2879d18bcf60282e611491ca61aadc7
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2025-02-03 15:40:42 +0530

    track updation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import logo from "@/public/logonew.png";
+import logo from "@/public/ic2sdtlogo.PNG";
 import nitdLogo from "@/public/nitdel-logo.jpg"  
 import ieee from "@/public/ieee_logo.png"
 import Link from "next/link";
